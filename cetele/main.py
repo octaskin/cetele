8b64bbaf2090950d78ada8e25a8d4457a101f4dd
octaskin <orhuncenktaskin@gmail.com>
2023-03-02 22:04:16 +0300

    more refactoring

--- cetele/main.py
+++ cetele/main.py
@@ -4,42 +4,25 @@ import json
 from csv import reader
 from pathlib import Path
 
-config_file = Path.home().joinpath(".config/cetele/config.json")
-if config_file.exists():
-    with open(config_file, "r", encoding="utf-8") as file:
-        state_file_path = json.load(file)["state_file_path"]
-else:
-    exit("Please define a path on the configuration file.")
-
-file_path = Path.home().joinpath(state_file_path)
-
-
-def last_day_of_month(any_day):
-    # credit: https://stackoverflow.com/a/13565185/13426912
-    # The day 28 exists in every month. 4 days later, it's always next month
-    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
-    # subtracting the number of the current day brings us back one month
-    return next_month - datetime.timedelta(days=next_month.day)
-
 
 class State:
     edit_actions = {"E": "edit", "D": "delete", "Q": "quit"}
+    edit_prompt = "You want to [E]dit or [D]elete an entry, Q[uit]?: "
 
     def __init__(self):
         self.fpath = self.read_config()
-        self.state = self.read()
+        self.content = self.read()
 
-    def read_config(self):
+    def read_config(self) -> Path:
         config_file = Path.home().joinpath(".config/cetele/config.json")
         if config_file.exists():
             with open(config_file, "r", encoding="utf-8") as file:
                 state_file_path = json.load(file)["state_file_path"]
         else:
             exit("Please define a path on the configuration file.")
-
         return Path.home().joinpath(state_file_path)
 
-    def read(self):
+    def read(self) -> list:
         logging.debug("Reading the state file.")
         with open(self.fpath, "r") as file:
             out = [r for r in reader(file, delimiter=",")]
@@ -47,58 +30,54 @@ class State:
 
     def write(self):
         res = ""
-        for row in self.state:
+        for row in self.content:
             res += ",".join(row) + "\n"
-
         with open(self.fpath, "w") as file:
             file.write(res)
 
     def edit(self):
         print(self)
-        action = self.edit_actions.get(
-            input(f"You want to [E]dit or [D]elete an entry, Q[uit]?: ").capitalize(),
-            None,
-        )
+        action = self.edit_actions.get(input(self.edit_prompt).capitalize(), None)
         if not action:
             exit("Unexpected argument!")
         elif action == "Q":
             exit("bye...")
         logging.debug(f"You chose {action}.")
         idx = int(input(f"Which entry you want to {action}: "))
-        print(f"{action}ing: {','.join(self.state[idx])}")
+        print(f"{action}ing: {','.join(self.content[idx])}")
         match action:
             case "edit":
-                if len(self.state[idx][1:]) > 1:
+                if len(self.content[idx][1:]) > 1:
                     exit(
                         "This is a parent enrty, editing this is not implemented yet..."
                     )
                 else:
-                    self.state[idx][1] = f"{float(input()):.2f}"
-                    print(f"New value: {','.join(self.state[idx])}")
+                    self.content[idx][1] = f"{float(input()):.2f}"
+                    print(f"New value: {','.join(self.content[idx])}")
             case "delete":
-                if input(f"Deleting: {','.join(self.state[idx])} [y\\n]") == "y":
-                    del self.state[idx]
-                    for i, row in enumerate(self.state):
-                        if len(row) > 2 and self.state[idx][0] in row:
+                if input(f"Deleting: {','.join(self.content[idx])} [y\\n]") == "y":
+                    del self.content[idx]
+                    for i, row in enumerate(self.content):
+                        if len(row) > 2 and self.content[idx][0] in row:
                             logging.debug(
-                                f"Removing child from parent {self.state[i][0]}"
+                                f"Removing child from parent {self.content[i][0]}"
                             )
-                            self.state[i].remove(self.state[idx][0])
+                            self.content[i].remove(self.content[idx][0])
                 else:
                     exit("Aborted.")
         self.write()
 
     def verify(self):
-        # currently unused
-        for i, row in enumerate(self.state):
-            if row_is_numeric(row):
-                rest = self.state[:i] + self.state[i + 1 :]
+        # Currently unused
+        for i, row in enumerate(self.content):
+            if self.row_is_child(row):
+                rest = self.content[:i] + self.content[i + 1 :]
                 whole = " ".join([" ".join(r) for r in rest])
                 if row[0] not in whole + " EUR2TRY":  # currency is an exception
                     print(f"{row[0]} is an orphan, please review or remove!")
 
     @staticmethod
-    def row_is_numeric(row: list[str]) -> bool:
+    def row_is_child(row: list[str]) -> bool:
         try:
             float(row[1])
             return True
@@ -109,8 +88,8 @@ class State:
         # TODO: test this, I'm not sure to print or return string
         logging.debug("Listing the state file.")
         res = ""
-        for i, row in enumerate(self.state):
-            if not row_is_numeric(row):
+        for i, row in enumerate(self.content):
+            if not self.row_is_child(row):
                 continue
             res += f"{i}: {','.join(row)}\n"
         return res[:-1]
@@ -118,7 +97,7 @@ class State:
 
 class Cetele:
     def __init__(self, state: State):
-        self.vals = self.form_state(state.state)
+        self.vals = self.form_state(state.content)
         self.state = state
 
     @staticmethod
@@ -126,7 +105,7 @@ class Cetele:
         res = {}
         for line in content:
             k = line[0]
-            if row_is_numeric(line):
+            if State.row_is_child(line):
                 res[k] = float(line[1])
             else:
                 res[k] = line[1:]
@@ -136,9 +115,17 @@ class Cetele:
         return res
 
     @staticmethod
-    def leftover_spenditure():
+    def last_day_of_month(any_day) -> datetime.date:
+        # credit: https://stackoverflow.com/a/13565185/13426912
+        # The day 28 exists in every month. 4 days later, it's always next month
+        next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
+        # subtracting the number of the current day brings us back one month
+        return next_month - datetime.timedelta(days=next_month.day)
+
+    @staticmethod
+    def leftover_spenditure() -> float:
         today = datetime.date.today()
-        days_left = last_day_of_month(today) - today
+        days_left = Cetele.last_day_of_month(today) - today
         return days_left.days * 15.00
 
     def calculate(self, k: str) -> float:
@@ -159,7 +146,7 @@ class Cetele:
         assert isinstance(v, float)
         return v
 
-    def __str__(self):
+    def __str__(self) -> str:
         cols = ["cetele", "savings", "flex", "dailies"]
         idx = ["status", "goal", "net"]
 
